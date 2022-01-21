819c6002c5b9889a0e4e84648cccf3e5b32a9b06
Brandao-lang <elijahbrandao@gmail.com>
2022-01-21 17:16:15 -0500

    problem text update

--- Linked Lists/Linked List Cycle/index.js
+++ Linked Lists/Linked List Cycle/index.js
@@ -1,5 +1,5 @@
 // Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
-// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+// There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.. 
 
 
 
